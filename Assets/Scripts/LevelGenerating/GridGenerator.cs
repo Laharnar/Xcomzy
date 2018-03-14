a5bf95ef4a81b8c44adea96f0a905368f65c58f0
TQ-PC\TQ <marko.laharnar@gmail.com>
2018-03-14 22:15:09 +0100

    wall models, sample map

--- Assets/Scripts/LevelGenerating/GridGenerator.cs
+++ Assets/Scripts/LevelGenerating/GridGenerator.cs
@@ -6,14 +6,19 @@ public class GridGenerator:MonoBehaviour {
 
     public Transform gridItem;
 
+    public int w = 20;
+    public int l = 20;
+
+    public Vector2 scale = new Vector2(1,1);
+
     private void Awake() {
-        MakeGrid(20, 20, transform.position.z);
+        MakeGrid(w, l, transform.position.z);
     }
 
     private void MakeGrid(int w, int l, float hpos) {
         for (int i = 0; i < w; i++) {
             for (int j = 0; j < l; j++) {
-                Instantiate(gridItem, new Vector3(i, hpos, j), new Quaternion(), transform);
+                Instantiate(gridItem, transform.position+new Vector3(i*scale.x, hpos, j*scale.y), new Quaternion(), transform);
             }
         }
     }
