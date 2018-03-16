1b5a80623ffe19b757c8971abd40fd14327df36c
Laharnar <marko.laharnar@gmail.com>
2018-03-16 10:41:45 +0100

    A* implementation

--- Assets/Scripts/GridSlot.cs
+++ Assets/Scripts/GridSlot.cs
@@ -30,8 +30,14 @@ public class GridSlot : MonoBehaviour {
 
     public static List<GridSlot> allSlots = new List<GridSlot>();
 
+    internal int id;
+    static int idCount = 0;
+
     // Use this for initialization
     void Awake() {
+        id = idCount;
+        idCount++;
+
         taken = null;
 
         Vector3 vec = new Vector3(transform.position.x, raycastFromHeight, transform.position.z);
@@ -44,7 +50,7 @@ public class GridSlot : MonoBehaviour {
         bool cast = Physics.Raycast(ray,
             out hit,
             raycastFromHeight - minHeight,
-            1<<LayerMask.NameToLayer(groundLayerName),
+            1 << LayerMask.NameToLayer(groundLayerName),
             QueryTriggerInteraction.Ignore
             );
         if (cast) {
@@ -70,10 +76,123 @@ public class GridSlot : MonoBehaviour {
     internal static GridSlot[] GetSlotsInRange(GridSlot slot, float range) {
         List<GridSlot> slots = new List<GridSlot>();
         for (int i = 0; i < allSlots.Count; i++) {
-            if (Vector3.Distance(allSlots [i].transform.position, slot.transform.position) <=range) {
+            if (Vector3.Distance(allSlots[i].transform.position, slot.transform.position) <= range) {
                 slots.Add(allSlots[i]);
             }
         }
         return slots.ToArray();
+    }
+
+    /// <summary>
+    /// Finds path on 1 floor grid.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="goal"></param>
+    /// <returns></returns>
+    public static GridSlot[] FindPathAStar(GridSlot start, GridSlot goal) {
+        if (start == null || goal == null) {
+            Debug.Log("FindPathAStar: start or goal is null.");
+            return new GridSlot[0];
+        }
+
+        // The set of nodes already evaluated
+        HashSet<GridSlot> closedSet = new HashSet<GridSlot>();
+
+
+        // The set of currently discovered nodes that are not evaluated yet.
+        // Initially, only the start node is known.
+        HashSet<GridSlot> openSet = new HashSet<GridSlot>();
+        openSet.Add(start);
+
+        // For each node, which node it can most efficiently be reached from.
+        // If a node can be reached from many nodes, cameFrom will eventually contain the
+        // most efficient previous step.
+        Dictionary<GridSlot, GridSlot> cameFrom = new Dictionary<GridSlot, GridSlot>();
+
+        // For each node, the cost of getting from the start node to that node.
+        // init as inifinity
+        Dictionary<GridSlot, float> gScore = new Dictionary<GridSlot, float>();
+
+        // For each node, the total cost of getting from the start node to the goal
+        // by passing by that node. That value is partly known, partly heuristic.
+        //map with default value of Infinity
+        Dictionary<GridSlot, float> fScore = new Dictionary<GridSlot, float>();
+
+        for (int i = 0; i < allSlots.Count; i++) {
+            gScore.Add(allSlots[i], float.PositiveInfinity);
+            fScore.Add(allSlots[i], float.PositiveInfinity);
+            cameFrom.Add(allSlots[i], null);
+        }
+
+        // The cost of going from start to start is zero.
+        gScore[start] = 0f;
+
+        // For the first node, that value is completely heuristic.
+        fScore[start] = HeuristicCostEstimate(start, goal);
+        while (openSet.Count > 0) {
+            // the node in openSet having the lowest fScore[] value
+            GridSlot current = null;
+            float minFScore = float.MaxValue;
+            for (int i = 0; i < allSlots.Count; i++) {
+                if (fScore[allSlots[i]] < minFScore) {
+                    minFScore = fScore[allSlots[i]];
+                    current = allSlots[i];
+                }
+            }
+            if (current == goal)
+                return ReconstructPath(cameFrom, current);
+
+            openSet.Remove(current);
+            closedSet.Add(current);
+
+            // Assumes 1 layered grid.
+            GridSlot[] curNeighbors = new GridSlot[4]{// 4 neighbouring slots.
+                allSlots[current.id+1],
+                allSlots[current.id-1],
+                allSlots[current.id-20],
+                allSlots[current.id+20],
+            };
+            foreach (var neighbor in curNeighbors) {
+                if (neighbor == null)
+                    continue;
+                if (closedSet.Contains(neighbor))
+                    continue;       // Ignore the neighbor which is already evaluated.
+
+                if (openSet.Contains(neighbor)) // Discover a new node
+                    openSet.Add(neighbor);
+
+                // The distance from start to a neighbor
+                //the "dist_between" function may vary as per the solution requirements.
+                float tentative_gScore = gScore[current] + Vector3.Distance(current.transform.position, neighbor.transform.position);
+                if (tentative_gScore >= gScore[neighbor])
+                    continue;        // This is not a better path.
+
+                // This path is the best until now. Record it!
+                cameFrom[neighbor] = current;
+                gScore[neighbor] = tentative_gScore;
+                fScore[neighbor] = gScore[neighbor] + HeuristicCostEstimate(neighbor, goal);
+            }
+        }
+        return new GridSlot[0];
+    }
+
+    private static float HeuristicCostEstimate(GridSlot neighbor, GridSlot goal) {
+        return goal.transform.position.x - neighbor.transform.position.x
+             + goal.transform.position.y - neighbor.transform.position.y;
+    }
+
+    private static GridSlot[] ReconstructPath(Dictionary<GridSlot, GridSlot> cameFrom, GridSlot current) {
+        int errLen = 10000;
+        List<GridSlot> total_path = new List<GridSlot>();
+        total_path.Add(current);
+        while (cameFrom.ContainsKey(current) && errLen > 0){
+            errLen--;
+            current = cameFrom[current];
+            total_path.Add(current);
+        }
+        if (errLen <= 0) {
+            Debug.Log("Error infi loop, cyclic path");
+        }
+        return total_path.ToArray();
     }
 }
